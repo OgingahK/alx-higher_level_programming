96b9edbf2e57447f20984fb4a8d49ee04594dd6c
OgingahK <kayceeklain@gmail.com>
2024-03-18 13:29:12 -0700

    Maximum Number

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    if my_list == 0:
+        return None
+
+    max1 = my_list[0]
+    for x in my_list:
+        if x > max1:
+            max1 = x
+
+    return max1
